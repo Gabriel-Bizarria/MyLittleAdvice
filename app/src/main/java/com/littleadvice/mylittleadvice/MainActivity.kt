84dd542135917dba06c84bcf1339949cd416c730
Gabriel Bizarria Santos <gabriel.santos@poatek.com>
2022-04-01 16:24:40 -0300

    First commit

---
+++ app/src/main/java/com/littleadvice/mylittleadvice/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.littleadvice.mylittleadvice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.littleadvice.mylittleadvice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+    private val binding get() = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.container_fragments)
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.dailyAdviceFragment,
+                R.id.randomFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
---
+++ app/src/main/java/com/littleadvice/mylittleadvice/utils/constants.kt
@@ -0,0 +1,4 @@
+package com.littleadvice.mylittleadvice.utils
+
+const val ADVICES_NUMBER = 224
+const val ADVICES_BASE_URL = "https://api.adviceslip.com/advice"
