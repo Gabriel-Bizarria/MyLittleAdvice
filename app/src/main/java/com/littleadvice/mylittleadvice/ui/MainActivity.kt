435c69fdda156411b012653e1d99683104ad79ac
Gabriel Bizarria Santos <gabriel.santos@poatek.com>
2022-04-04 18:07:24 -0300

    Add: splash screen

--- app/src/main/java/com/littleadvice/mylittleadvice/ui/MainActivity.kt
+++ app/src/main/java/com/littleadvice/mylittleadvice/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.littleadvice.mylittleadvice.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         _binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
